a763b9d8e20e4a77e6c84738c169381187de6aed
iBug <git@ibugone.com>
2022-03-13 00:27:47 +0800

    Embed Roboto Mono

--- ustclug-website/build.sh
+++ ustclug-website/build.sh
@@ -2,7 +2,7 @@
 
 BASE_EXTRA="$(dirname "$0")/files/var/www/_extra"
 
-# Fetch Font Awesome
+# Font Awesome Pro
 URL="$(curl -s https://registry.npmjs.org/fapro-free/ | jq -r '.versions["5.15.4"].dist.tarball')"
 PACKAGE=package.tgz
 curl -so "$PACKAGE" "$URL"
@@ -13,7 +13,7 @@ mkdir -p "$BASE_EXTRA"/fontawesome
 mv package/{css,js,webfonts} "$BASE_EXTRA"/fontawesome/
 rm -rf package package.tgz
 
-# Fetch Open Sans
+# Open Sans
 BASEURL="https://fontfacekit.github.io/open-sans"
 BASEDIR="$BASE_EXTRA"
 curl -so "$BASEDIR/open-sans.css" "$BASEURL/open-sans.css"
@@ -22,7 +22,7 @@ for f in $(grep -Po "url\(\K[^)]+" "$BASEDIR/open-sans.css"); do
   curl -sLo "$BASEDIR/${f%%\?*}" "$BASEURL/$f"
 done
 
-# Fetch Roboto
+# Roboto
 BASEURL="https://fontfacekit.github.io/roboto"
 BASEDIR="$BASE_EXTRA"
 curl -so "$BASEDIR/roboto.css" "$BASEURL/roboto.css"
@@ -30,3 +30,9 @@ for f in $(grep -Po 'url\("?\K[^")]+' "$BASEDIR/roboto.css"); do
   mkdir -p "$BASEDIR/$(dirname "$f")"
   curl -sLo "$BASEDIR/${f%%\?*}" "$BASEURL/$f"
 done
+
+# Roboto Mono
+URL="https://download.ibugone.com/fonts/RobotoMono.zip"
+curl -so package.zip "$URL"
+unzip -od "$BASE_EXTRA"/roboto-mono/ -x package.zip
+rm -f package.zip
